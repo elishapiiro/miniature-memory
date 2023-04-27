18e3050d1362e68c79c06b73754af9147f631d2a
Eli Shalom <71207769+elishapiiro@users.noreply.github.com>
2023-04-27 09:15:58 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -12,7 +12,6 @@ class User:
     password: str
     ssn: str
     home_address: Dict[str, str]
-    age: int
 
 client = pymongo.MongoClient(os.environ['MONGODB_CONNECTION_STRING'])
 db = client.mydatabase
@@ -22,11 +21,10 @@ raw_users = []
 for user in users_collection.find():
     raw_users.append(user)
 
-# Load the encrypted users into User objects
 users = []
-for user in raw_users:
+for raw_user in raw_users:
     user_dict = {}
-    for key, value in encrypted_user.items():
+    for key, value in raw_user.items():
         if key == '_id':
             # Convert the ObjectId to a string
             value = str(value)
